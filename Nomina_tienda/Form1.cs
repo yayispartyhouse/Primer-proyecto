24ea430d6b66424d165fb4df524697ebd9e24a60
yayispartyhouse <48001519+yayispartyhouse@users.noreply.github.com>
2020-01-20 19:12:29 -0600

    Agregue un Boton Con un Message.box tuga.

--- Nomina_tienda/Form1.cs
+++ Nomina_tienda/Form1.cs
@@ -14,7 +14,19 @@ namespace Nomina_tienda
     {
         public Form1()
         {
+            
+             
             InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Tugfa");
         }
     }
 }
